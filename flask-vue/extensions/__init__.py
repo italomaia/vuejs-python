1413500c3b5843ad48e5986f72c63bf57b546220
Italo Maia <italo.maia@live.com>
2016-02-06 23:24:36 -0300

    defining our instances

--- flask-vue/extensions/__init__.py
+++ flask-vue/extensions/__init__.py
@@ -1,3 +1,9 @@
 from .database import db
 from .api import api as rest_api
 from .schemas import ma
+from .webassets import assets
+from flask_admin import Admin
+from flask_debugtoolbar import DebugToolbarExtension
+
+toolbar = DebugToolbarExtension()
+admin = Admin(name='VueJS blog', template_mode='bootstrap3')
