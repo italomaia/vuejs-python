2e8c8ce647a7fde0975999cd314575a62c910af6
Italo Maia <italo.maia@live.com>
2016-02-07 23:46:41 -0300

    adding the async command to manage.py. one can use it to run the socketio webserver

--- flask-vue/manage.py
+++ flask-vue/manage.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from flask_script import Manager
+from extensions.socketio import AsyncServer
 
 import commands
 
@@ -8,9 +9,12 @@ if __name__ == "__main__":
     from main import app_factory
     import config
 
+    # socket io requires a special runserver setup
     manager = Manager(app_factory)
     manager.add_option("-n", "--name", dest="app_name", required=False, default=config.project_name)
     manager.add_option("-c", "--config", dest="config", required=False, default=config.Dev)
+
+    manager.add_command("async", AsyncServer())
     manager.add_command("test", commands.Test())
     manager.add_command("apps", commands.Apps())
     manager.add_command("db-create", commands.CreateDB())
